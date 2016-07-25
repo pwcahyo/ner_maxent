012ec164ba9bc4ba7033c96e3ba05cc7cc41c5b4
insomniart <pwcahyo@gmail.com>
2016-07-25 17:04:47 +0700

    fix preprocessing 2

--- regexp.py
+++ regexp.py
@@ -3,11 +3,18 @@
 #kumpulang fungsi reguler expression
 import re
 
+# get word
 w = re.compile(r"(.*)/") 
+# get label
 lbl = re.compile(r"/(.*)") 
+# get number
 num = re.compile(r"(.*)/int") 
+# get tag # @ &text
 tag = re.compile(r"(#\S+)|(@\S+)|(&\S+)|(')")
+#get symbol no slice /
 symbol_train = re.compile(r"[^A-Za-z!/]")
-symbol_ner = re.compile(r"[^A-Za-z!]")
+#get symbol
+symbol_ner = re.compile(r"[^A-Za-z]")
 #tag = re.compile(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)")
+# get url
 url = re.compile(r"^https?:\/\/.*[\r\n]*", flags=re.MULTILINE)
